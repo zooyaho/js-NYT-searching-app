96f47a9d1955172b18b3b90df55ea92389732b25
zooyaho <jiwoo1268@naver.com>
2022-08-01 02:36:28 +0900

    Feat: Add display of recent searches
    최대 5개 최근 검색어를 input에 focus시 활성화

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ import _ from "lodash";
 const NEWS_URL =
   "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=keyword&page=pageNum&sort=newest&api-key=FdwAiEGYCwpc9manUy55RoDgPUtMOWtX";
 let inputTimeId;
-let keyword;
 let newsData; // api로 받아온 데이터
 let pageNum = 1;
+let searchHistory = []; // 최근 검색어 최대 5개 저장
+let keyword; // 검색어
 
 const inputEl = document.querySelector("#keyword");
 const formEl = document.querySelector(".search-form");
 const articleEl = document.querySelector(".article");
 const searchBtnEl = document.querySelector("#btn-search");
+const keywordBodyEl = document.querySelector(".keyword-body");
+const keywordWrapEl = document.querySelector(".keyword-wrap");
 
 /* api호출하여 data 반환하는 함수 */
 function getData(url) {
@@ -51,12 +54,40 @@ async function addArticleHtml(pageNumber) {
   articleEl.innerHTML = newsList.join("");
 }
 
+/* keywordBody에 최근 검색어 html 추가하는 함수 */
+function addkeywordHistoryHtml() {
+  searchHistory.reverse();
+  const keywordList = [];
+  keywordList.push("<ul>");
+  for (let i = 0; i < searchHistory.length; i++) {
+    keywordList.push(`
+      <li>${searchHistory[i]}</li>
+    `);
+  }
+  keywordList.push("</ul>");
+  keywordBodyEl.innerHTML = keywordList.join("");
+}
+
+/* 검색어 searchHistory에 저장하는 함수 */
+function AddSearchHistory(keyword) {
+  searchHistory.reverse();
+  // 검색어 최대 5개까지 저장
+  if (searchHistory.length < 5) {
+    searchHistory[searchHistory.length] = keyword;
+  } else {
+    searchHistory.shift();
+    searchHistory = [...searchHistory, keyword];
+  }
+  addkeywordHistoryHtml();
+}
+
 /* Search Button 클릭 시 api호출하는 핸들러 함수 */
 const clickBtnHandler = () => {
   if (inputEl.value !== "") {
     keyword = inputEl.value;
     pageNum = 1;
     addArticleHtml(pageNum);
+    AddSearchHistory(inputEl.value);
   } else {
     alert("검색어를 입력하세요.");
   }
@@ -70,16 +101,25 @@ const unchangedHendler = (e) => {
       keyword = e.target.value;
       pageNum = 1;
       addArticleHtml(pageNum);
-      console.log(e.target.value);
+      AddSearchHistory(e.target.value);
+      keywordWrapEl.style.display = "block";
     }
-  }, 1000);
+  }, 2000);
 };
 
-inputEl.addEventListener("keyup", unchangedHendler);
-searchBtnEl.addEventListener("click", clickBtnHandler);
 formEl.addEventListener("submit", (e) => {
   // 재로딩 방지
   e.preventDefault();
+});
+searchBtnEl.addEventListener("click", clickBtnHandler);
+inputEl.addEventListener("keyup", unchangedHendler);
+inputEl.addEventListener("focus", () => {
+  if (searchHistory.length !== 0) {
+    keywordWrapEl.style.display = "block";
+  }
+});
+inputEl.addEventListener("blur", () => {
+  keywordWrapEl.style.display = "none";
 });
 window.addEventListener(
   "scroll",
