671fa6e7d15d2947ac55902b6bb1c9d63bff976c
zooyaho <jiwoo1268@naver.com>
2022-08-03 00:23:46 +0900

    Docs: Add descriptive comments
    설명 주석 추가

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ import _ from "lodash";
 import API_KEY from "./token";
 
 const NEWS_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=keyword&page=pageNum&sort=newest&api-key=${API_KEY}`;
-let inputTimeId;
+let inputTimeId; // input의 timeout id
 let newsData; // api로 받아온 데이터
-let pageNum = 1;
+let pageNum = 1; // 호출할 page
 let searchHistory = []; // 최근 검색어 최대 5개 저장
 let keyword; // 검색어
 let articleItemsList; // 기사 리스트들
 let starBtnsList; // 별 버튼 리스트들
 let newsHtmlList = []; // 추가할 html 문자열 리스트
-let bookmarkTrigger = false;
+let bookmarkTrigger = false; // 스크롤 이벤트 방지
 
 const inputEl = document.querySelector("#keyword");
 const formEl = document.querySelector(".search-form");
@@ -61,10 +61,10 @@ async function addArticleHtml() {
 
 /* 새로운 검색 시 기사 추가하는 함수 */
 async function keywordSearch() {
-  await addArticleHtml();
-  articleEl.innerHTML = newsHtmlList.join("");
-  articleItemsList = document.querySelectorAll(".article-item");
-  starBtnsList = document.querySelectorAll(".toggle-star");
+  await addArticleHtml(); // api 호출
+  articleEl.innerHTML = newsHtmlList.join(""); // articleEl에 기사 리스트 추가
+  articleItemsList = document.querySelectorAll(".article-item"); // articleItemsList 업데이트
+  starBtnsList = document.querySelectorAll(".toggle-star"); // starBtnsList 업데이트
 
   if (window.innerHeight > document.body.scrollHeight && pageNum === 1) {
     // 첫페이지가 화면의 높이를 모두 채우지 못한 경우 다음 페이지 api요청
@@ -75,15 +75,19 @@ async function keywordSearch() {
 
 /* 스크롤 바닥일때 기사 추가하는 함수 */
 async function scrollEndArticleHtml() {
-  await addArticleHtml();
+  await addArticleHtml(); // api 호출
+
+  // template요소를 사용하여 문자열을 노드객체로 변환
   const template = document.createElement("template");
   template.innerHTML = newsHtmlList.join("");
   const newListNode = template.content.children;
+
   for (let i = 0; i < newListNode.length; i++) {
+    // 추가 페이지의 뉴스기사 리스트 출력
     articleEl.appendChild(newListNode[i]);
   }
-  articleItemsList = document.querySelectorAll(".article-item");
-  starBtnsList = document.querySelectorAll(".toggle-star");
+  articleItemsList = document.querySelectorAll(".article-item"); // articleItemsList 업데이트
+  starBtnsList = document.querySelectorAll(".toggle-star"); // starBtnsList 업데이트
 }
 
 /* keywordBody에 최근 검색어 html 추가하는 함수 */
@@ -118,25 +122,26 @@ const clickBtnHandler = () => {
   if (inputEl.value !== "") {
     keyword = inputEl.value;
     pageNum = 1;
-    keywordSearch();
-    addSearchHistory(inputEl.value);
+    keywordSearch(); // 키워드 검색
+    addSearchHistory(inputEl.value); // 키워드 저장
   } else {
+    // 검색어가 빈값일 경우
     alert("검색어를 입력하세요.");
   }
 };
 
-/* 0.5초 동안 추가입력이 없으면 api호출하는 핸들러 함수 */
+/* 0.5초 동안 추가입력 없으면 api호출하는 핸들러 함수 */
 const unchangedHendler = (e) => {
   clearTimeout(inputTimeId);
   inputTimeId = setTimeout(() => {
     if (e.target.value !== "") {
       keyword = e.target.value;
       pageNum = 1;
-      keywordSearch();
-      addSearchHistory(e.target.value);
-      keywordWrapEl.style.display = "block";
+      keywordSearch(); // 키워드 검색
+      addSearchHistory(e.target.value); // 키워드 저장
+      keywordWrapEl.style.display = "block"; // 최근 검색어 영역 활성화
     }
-  }, 2000);
+  }, 500);
 };
 
 formEl.addEventListener("submit", (e) => {
@@ -144,18 +149,22 @@ formEl.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+/* 검색 버튼 클릭 시 api호출 */
 searchBtnEl.addEventListener("click", clickBtnHandler);
+/* 키보드로 검색 시 api호출 */
 inputEl.addEventListener("keyup", unchangedHendler);
-
+/* 검색 input focus일 경우 */
 inputEl.addEventListener("focus", () => {
   if (searchHistory.length !== 0) {
-    keywordWrapEl.style.display = "block";
+    keywordWrapEl.style.display = "block"; // 최근 검색어 영역 활성화
   }
 });
+/* 검색 input blur일 경우 */
 inputEl.addEventListener("blur", () => {
-  keywordWrapEl.style.display = "none";
+  keywordWrapEl.style.display = "none"; // 최근 검색어 영역 비활성화
 });
 
+/* 스크롤 감지 이벤트함수 */
 window.addEventListener(
   "scroll",
   _.throttle(() => {
@@ -163,18 +172,20 @@ window.addEventListener(
     let fullHeight = document.body.offsetHeight;
 
     // header 높이: 190px
-    // 스크롤이 바닥에 닿을 경우
+    // 스크롤이 바닥에 닿을 경우, 북마크 비활성화 시 api호출
     if (windowHeight + scrollY - 190 >= fullHeight && !bookmarkTrigger) {
-      console.log("스크롤 바닥!!!!");
       ++pageNum;
-      scrollEndArticleHtml();
+      scrollEndArticleHtml(); // api호출
     }
   }, 1000)
 );
+
+/* 즐겨찾기 토글 버튼 기능 */
 articleEl.addEventListener("click", (e) => {
   e.target.parentNode.classList.toggle("active");
 });
 
+/* 북마크 토글 버튼 기능 */
 toggleBookmarkEl.addEventListener("click", (e) => {
   const isActive = e.target.parentNode.classList.toggle("active");
 
@@ -183,6 +194,7 @@ toggleBookmarkEl.addEventListener("click", (e) => {
     articleEl.innerHTML = ""; // reset
     for (let i = 0; i < starBtnsList.length; i++) {
       if (starBtnsList[i].classList.contains("active")) {
+        // 즐겨찾기 버튼이 활성화된 기사들만 출력
         articleEl.appendChild(starBtnsList[i].parentNode.parentNode);
       }
     }
